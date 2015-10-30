aabce218e69c6b6d37837fa8ce0468e5e745c67d
Tomaroka <yvzcevik@gmail.com>
2015-10-30 20:24:44 +0200

    Main thread will wait till connection

--- odev04/message_server.py
+++ odev04/message_server.py
@@ -1,1 +1,11 @@
 import socket
+
+s = socket.socket()
+host = "127.0.0.1"
+port = 12345
+s.bind ((host, port))
+s.listen(5)
+while True:
+    print "Waiting for connection"
+    c, addr = s.accept()
+    print 'Got a connection from', addr
