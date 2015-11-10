9683af89db3a8aa571785b33504afbaf438b7637
Tomaroka <yvzcevik@gmail.com>
2015-11-10 16:44:35 +0200

    starting to reading from file

--- odev03/caesar_thread.py
+++ odev03/caesar_thread.py
@@ -1,13 +1,18 @@
 import threading
 import time 
+import Queue
 
 class Read_Thread (threading.Thread):
-    def __init__(self, threadID, name):
+    def __init__(self, threadID, name, read_size, read_queue, text_file):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
+        self.read_size = read_size
+        self.read_queue = read_queue
+        self.text_file = text_file
     def run(self):
-        #job
+        read_block = self.text_file.read(self.read_size)
+        
     
 
 class Crypter_Thread (threading.Thread):
