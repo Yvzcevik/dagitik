ad96eed4fabe5fe60e945c6853f94a09936ecfda
Tomaroka <yvzcevik@gmail.com>
2015-11-10 15:01:32 +0200

    Creating Threads

--- odev03/caesar_thread.py
+++ odev03/caesar_thread.py
@@ -1,1 +1,28 @@
+import threading
+import time 
 
+class Read_Thread (threading.Thread):
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+    def run(self):
+        #job
+    
+
+class Crypter_Thread (threading.Thread):
+    def ___init__(self, threadID,name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+    def run(self):
+        #job
+    
+class Writer_Thread (threading.Thread):
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+    def run(self):
+        #job
+    
