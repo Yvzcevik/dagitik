aed05e62186b8d38b4cc05b22499eca79ef8963f
Tomaroka <yvzcevik@gmail.com>
2015-11-12 14:29:52 +0200

    Last Changes

--- odev03/caesar_thread.py
+++ odev03/caesar_thread.py
@@ -11,12 +11,17 @@ class Read_Thread (threading.Thread):
         self.read_queue = read_queue
         self.text_file = text_file
     def run(self):
+        block_num = 1        
         read_block = self.text_file.read(self.read_size)
-        
+        if read_block == "" :
+            break
+        composed_block = (read_block , block_num)
+        self.read_queue.put(read_block)
+         
     
 
 class Crypter_Thread (threading.Thread):
-    def ___init__(self, threadID,name):
+    def ___init__(self, threadID,name, read_queue, alphabet):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
